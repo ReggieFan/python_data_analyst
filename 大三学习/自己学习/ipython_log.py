312d6e82d5d1009d0233ea62843bdccf6f29e15f
scut-fintech <btxu@scut.edu.cn>
2019-08-22 21:53:47 +0800

    'python_data_analyst'

---
+++ 大三学习/自己学习/ipython_log.py
@@ -0,0 +1,109 @@
+# IPython log file
+
+a =[1,2,3]
+a.<Tab>
+D:
+get_ipython().run_line_magic('pinfo', 'a')
+a =[1,2,3]
+a
+get_ipython().run_line_magic('pinfo', 'a')
+get_ipython().run_line_magic('run', 'D:tom.py')
+get_ipython().run_line_magic('pinfo', 'a')
+get_ipython().run_line_magic('paste', '')
+get_ipython().run_line_magic('cpaste', '')
+import matplotlib as plt
+lzx=plt.imread('D:\图片\heshui.jpg')
+imshow(lzx)
+import matplotlib.pylab as plt
+lzx=plt.imread('D:\图片\heshui.jpg')
+imshow(lzx)
+import matplotlib.pylab as plt
+lzx=plt.imread('D:\图片\heshui.jpg')
+plt.show()
+import matplotlib.pylab as plt
+lzx=plt.imread('D:\图片\heshui.jpg')
+plt.show(lzx)
+import matplotlib.pylab as plt
+lzx=plt.imread('D:\图片\heshui.jpg')
+plt.show()
+import matplotlib.pylab as plt
+plt.imread('D:\图片\heshui.jpg')
+plt.show()
+import matplotlib.pylab as plt
+plt.imread('D:\图片\heshui.jpg')
+plt.show()
+get_ipython().run_line_magic('logstart', '')
+def buhao():
+    a=[1,2,3,4,5,6]
+    c=[2,3,4,5,6,7]
+    for i in a:
+        c.append(i)
+    print (c)
+def zhongdeng():
+    a=[1,2,3,4,5,6]
+    c=[2,3,4,5,6,7]
+    c+=a
+    print(c)
+def hao():
+    a=[1,2,3,4,5,6]
+    c=[2,3,4,5,6,7]
+    c.entend(a)
+    print (c)
+get_ipython().run_line_magic('timeit', 'buhao()')
+get_ipython().run_line_magic('timeit', 'zhongdeng()')
+get_ipython().run_line_magic('timeit', 'hao()')
+def buhao():
+    a=[1,2,3,4,5,6]
+    c=[2,3,4,5,6,7]
+    for i in a:
+        c.append(i)
+#     print (c)
+def zhongdeng():
+    a=[1,2,3,4,5,6]
+    c=[2,3,4,5,6,7]
+    c+=a
+#     print(c)
+def hao():
+    a=[1,2,3,4,5,6]
+    c=[2,3,4,5,6,7]
+    c.entend(a)
+#     print (c)
+get_ipython().run_line_magic('timeit', 'buhao()')
+get_ipython().run_line_magic('timeit', 'zhongdeng()')
+get_ipython().run_line_magic('timeit', 'hao()')
+get_ipython().run_line_magic('timeit', 'buhao()')
+get_ipython().run_line_magic('timeit', 'zhongdeng()')
+get_ipython().run_line_magic('timeit', 'hao()')
+def buhao():
+    a=[1,2,3,4,5,6]
+    c=[2,3,4,5,6,7]
+    for i in a:
+        c.append(i)
+#     print (c)
+def zhongdeng():
+    a=[1,2,3,4,5,6]
+    c=[2,3,4,5,6,7]
+    c+=a
+#     print(c)
+def hao():
+    a=[1,2,3,4,5,6]
+    c=[2,3,4,5,6,7]
+    c.extend(a)
+#     print (c)
+get_ipython().run_line_magic('timeit', 'buhao()')
+get_ipython().run_line_magic('timeit', 'zhongdeng()')
+get_ipython().run_line_magic('timeit', 'hao()')
+import matplotlib.pylab as plt
+lzx=plt.imread('D:\图片\heshui.jpg')
+plt.imshow(lzx)
+import matplotlib.pylab as plt
+lzx=plt.imread('D:\图片\heshui.jpg')
+figure(figsize(5,5))
+plt.imshow(lzx)
+import matplotlib.pylab as plt
+lzx=plt.imread('D:\图片\heshui.jpg')
+plt.figure(figsize(5,5))
+plt.imshow(lzx)
+import matplotlib.pylab as plt
+lzx=plt.imread('D:\图片\heshui.jpg')
+plt.imshow(lzx)
